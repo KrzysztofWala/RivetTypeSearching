f90edf92af75c47765b75d031803deb87984f356
home <lazy44@op.pl>
2022-11-01 15:37:58 +0100

    221101_ver3_working

--- venv/RivetSearching.py
+++ venv/RivetSearching.py
@@ -4,54 +4,52 @@ import xlsxwriter
 # from RivetsFunctions import *
 import RivetsFunctions as rf
 
-# Creating dictionary variables
-dfm_dict = {}
-dfm_dict_temp = {}
-dfs_dict = {}
 Spot_material_dict={}
+dfm_list = []
+dfm_list_temp = []
+dfs_list = []
 
 # Import data
 dfs, dfm = rf.ImportParam()
-print(dfs)
-print(dfm)
 
-
+licz=0
 # Creating material dictionary - dfm_dict{} - collection all rivet type assigned to metal sheets combinations
 # Iteration on dfm rows
 for row in dfm.itertuples():
-    dfm_dict_temp = {}
-    # Adding combination meterial:tickness to dictionary
+    dfm_list_temp = []
+    # Adding combination meterial:tickness to list
     for i in range(2,7,2):
         # Checking if excel cell is not empty
         if pd.isnull(row[i])==False:
-            dfm_dict_temp[row[i]] = row[i+1]
+            dfm_list_temp.append([row[i],row[i+1]])
     # Alphabetical sort
-    dfm_dict_temp=dict(sorted(dfm_dict_temp.items()))
-    # Checking if dfm_dict_temp is not empty
-    if dfm_dict_temp != {}:
-        # Adding dfm_dict_temp if not empty to dfm_dict
-        dfm_dict[row[1]]=dfm_dict_temp
+    dfm_list_temp.sort()
 
-print(dfm_dict)
+    # Checking if dfm_list_temp is not empty
+    if len(dfm_list_temp) > 0:
+        # Adding dfm_list_temp if not empty to dfm_list
+        dfm_list.append([row[1],dfm_list_temp])
 
 # Iteration on all rivets in spots list
 for row in dfs.itertuples():
     # Creating material dictionary for each spot - dfs_dict
-    dfs_dict = {}
+    dfs_list = []
     for i in range(2,7,2):
 
         # Checking if excel cell is not empty
         if pd.isnull(row[i])==False:
-            dfs_dict[row[i]] = row[i+1]
-
+            dfs_list.append([row[i], row[i + 1]])
     # Alphabetical sort
-    dfs_dict = dict(sorted(dfs_dict.items()))
-    # Searching for rivet type (dfs_dict) in meterial dictionary (dfm_dict)
-    for key, value in dfm_dict.items():
-        if value==dfs_dict:
+    dfs_list.sort()
 
-            # Creating dictionary with Spot name and matching material
-            Spot_material_dict[row[1]] = key
+    # # Searching for rivet type (dfs_dict) in meterial dictionary (dfm_list)
+    match=False
+    for x in range (0, len(dfm_list)):
+        if dfs_list==dfm_list[x][1]:
+            Spot_material_dict[row[1]] = dfm_list[x][0]
+            match=True
+    if match==False:
+        Spot_material_dict[row[1]] = 'not found'
 
 
 # Save result to file - dictionary (Spot_name:Rivet_type)
